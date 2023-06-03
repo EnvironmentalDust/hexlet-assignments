a797acdb044788a3e8c598a1f4c8e2dbd33e14a3
@hexlet/cli <support@hexlet.io>
2023-06-04 04:09:38 +0500

    submit java-collections-ru/generics

---
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+// BEGIN
+public class App {
+
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
+        List<Map<String, String>> result = new ArrayList<>();
+
+        for (Map<String, String> book: books) {
+            if (containsAll(book, where)) {
+                result.add(book);
+            }
+        }
+
+        return result;
+    }
+
+    static boolean containsAll(Map<String, String> mapA, Map<String, String> mapB) {
+        return mapA.entrySet().containsAll(mapB.entrySet());
+    }
+
+}
+//END

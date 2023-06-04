dcaac6740cad880725f2b2cf4cb6ca484e51414f
@hexlet/cli <support@hexlet.io>
2023-06-04 05:07:54 +0500

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -18,16 +18,23 @@ class AppTest {
         assertThat(App.take(numbers1, 3))
                 .size().isEqualTo(3);
 
-        assertThat(App.take(numbers1, 999))
-                .size().isEqualTo(4);
-
         List<Integer> expected1 = new ArrayList<>(Arrays.asList(1, 2));
         assertThat(App.take(numbers1, 2))
                 .isEqualTo(expected1);
 
-        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3));
-        assertThat(App.take(numbers1, 3))
+        List<Integer> numbers2 = new ArrayList<>();
+        List<Integer> expected2 = new ArrayList<>();
+
+        assertThat(App.take(numbers2, 30))
                 .isEqualTo(expected2);
+
+        assertThat(App.take(numbers1, 999))
+                .size().isEqualTo(4);
+
+
+        List<Integer> expected3 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        assertThat(App.take(numbers1, 3))
+                .isEqualTo(expected3);
 
 
         // END
